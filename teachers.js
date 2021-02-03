7bb1995ee95d87a24578970efdc6e5ac9dd4ae89
pedrosouza423 <pedro.souza4237@gmail.com>
2021-02-03 18:41:31 -0300

    Foi criada a página show

--- teachers.js
+++ teachers.js
@@ -1,6 +1,24 @@
 const fs = require('fs')
 const data = require('./data.json')
 
+//show
+exports.show = function(req,res){
+    const {id} = req.params
+
+    const foundTeacher = data.teachers.find(function(teacher){
+        return teacher.id == id
+    })
+    
+    if(!foundTeacher) return res.send("Teacher not found")
+
+    const teacher = {
+        ...foundTeacher
+    }
+
+    res.render('teachers/show', {teacher})
+
+}
+
 //create
 exports.post = function(req,res){
     const keys = Object.keys(req.body)
